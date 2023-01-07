0866e6040cbf43c91a0bbd6d6edcea466e2729c1
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-01-07 21:27:10 +0900

    블루프린트 -> C++ 전환 - 1 -

---
+++ Source/Unknow_Project/Private/C_Ladder.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_Ladder.h"
+
+// Sets default values
+AC_Ladder::AC_Ladder()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AC_Ladder::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AC_Ladder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Unknow_Project/Unknow_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Unknow_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unknow_Project, "Unknow_Project" );
